bc032dc6aee307900e5805923c7f5cde712b3d0c
DOUGLAS DA SILVA FERREIRA <202307272922@alunos.ibmec.edu.br>
2025-05-06 16:58:42 -0300

    alteração na pergunta 1

--- projetoilhaprimeira/app/build.gradle.kts
+++ projetoilhaprimeira/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.projeto_ilha_primeira"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.projeto_ilha_primeira"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
